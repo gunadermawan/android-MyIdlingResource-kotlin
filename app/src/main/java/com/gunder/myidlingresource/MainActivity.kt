99625f83042d2405ac2174c76e69d39cfb6d4923
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-11-04 10:09:00 +0700

    logic untuk memulai threading aplikasi

--- app/src/main/java/com/gunder/myidlingresource/MainActivity.kt
+++ app/src/main/java/com/gunder/myidlingresource/MainActivity.kt
@@ -2,10 +2,25 @@ package com.gunder.myidlingresource
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.gunder.myidlingresource.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+//    binding
+    private lateinit var mainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+        mainBinding.button.setOnClickListener {
+            delay1()
+        }
+    }
+
+    private fun delay1() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            mainBinding.textView.text = getString(R.string.delay1)
+        },2000)
     }
 }
