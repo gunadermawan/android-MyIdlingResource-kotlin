22d82e2d49b6e8f4a37c158bad0e133261b3ded5
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-11-04 10:24:40 +0700

    membuat class idlingResource untuk mengangani asyncronus process

--- app/src/main/java/com/gunder/myidlingresource/EspressoIdlingResource.kt
+++ app/src/main/java/com/gunder/myidlingresource/EspressoIdlingResource.kt
@@ -1,4 +1,20 @@
 package com.gunder.myidlingresource
 
+import androidx.test.espresso.IdlingResource
+import androidx.test.espresso.idling.CountingIdlingResource
+
 object EspressoIdlingResource {
+    private val RESOURCE: String = "GLOBAL"
+    private val espressoTestidlingResource = CountingIdlingResource(RESOURCE)
+    fun increment() {
+        espressoTestidlingResource.increment()
+    }
+
+    fun decrement() {
+        espressoTestidlingResource.decrement()
+    }
+
+    fun getEspressoIdlingResource(): IdlingResource {
+        return espressoTestidlingResource
+    }
 }
